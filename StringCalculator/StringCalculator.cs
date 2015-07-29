bf9fe3418879c7c9959ff84255a436ff10fa30a1
Turo Nylund <turo.nylund@outlook.com>
2015-07-29 21:11:48 +0300

    - Added first test case. - Noticed that Add is going to do too much, functions should do only one thing. - Added Extract -test cases but noticed that is going to know too much. So there is two functions. Extract that manipulates string and Add that should do calculations.
    Some points I learned and how to go forward:
    - Start again with Add test case.
    - There is not necessary do another public method... could be private first in StringCalculator class. At now, it doesn't matter how much Add function does. We can separate functions that manipulate strings to other class later. In refactoring phase.

---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        public string Add(string input)
+        {
+            return "operator not supported";
+        }
+
+        public List<int> ExtractNumbers(string input)
+        {
+            List<int> integers = new List<int>();
+
+            var matchCollection = Regex.Matches(input, @"^-?\d+");
+
+            return integers;
+        }
+    }
+}
